386770cd2a2def86e153dfda86f0279aefdbe51e
Harry Min <happyjet123@gmail.com>
2020-01-16 22:24:03 +0000

    Basic text area done

---
+++ code_text.py
@@ -0,0 +1,51 @@
+import wx
+import wx.stc
+
+
+class CodeText(wx.stc.StyledTextCtrl):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.init_settings()
+
+    def init_settings(self):
+        font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, 'Source Code Pro')
+        self.StyleSetFont(wx.stc.STC_STYLE_DEFAULT, font)
+
+        self.SetLexer(wx.stc.STC_LEX_PYTHON)
+        self.SetMarginType(1, wx.stc.STC_MARGIN_NUMBER)
+        self.SetCaretLineVisible(True)
+
+        self._set_default_indentation_settings()
+        self._edit_keybinds()
+
+    def _set_default_indentation_settings(self):
+        self.SetIndent(4)
+        self.SetTabWidth(4)
+        self.SetBackSpaceUnIndents(True)
+        self.SetTabIndents(True)
+        self.SetUseTabs(False)
+        self.SetIndentationGuides(True)
+
+    def _edit_keybinds(self):
+
+        # I could `self.CmdKeyClearAll()` and then only bind the keybinds that are necessary
+
+        self.CmdKeyClear(ord('U'), wx.stc.STC_SCMOD_CTRL)
+        self.CmdKeyClear(ord('U'), wx.stc.STC_SCMOD_CTRL | wx.stc.STC_SCMOD_SHIFT)
+
+        self.Bind(wx.EVT_KEY_DOWN, self.key_pressed)
+
+    def key_pressed(self, event):
+        # print(event, event.GetKeyCode())
+        key_code = event.GetKeyCode()
+
+        if key_code == wx.WXK_RETURN and not wx.GetKeyState(wx.WXK_SHIFT):
+            self.newline_with_indentation()
+        else:
+            event.Skip()
+
+    def newline_with_indentation(self):
+        indentation = self.GetLineIndentation(self.GetCurrentLine())
+        self.NewLine()
+        self.AddText(' ' * indentation)
--- editor.py
+++ editor.py
@@ -1,22 +1,20 @@
 import wx
-# import wx.lib.agw.flatnotebook as flatnotebook
 import wx.aui
 
-# wx.aui almost cenrtainly contains a dockable widget
-# Could have one dockwidget for the whole editor area
+from code_text import CodeText
 
 
 class EditorPage(wx.Panel):
-    
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.create_widgets()
-    
+
     def create_widgets(self):
 
-        self.code_text = wx.TextCtrl(self)
+        # self.code_text = wx.TextCtrl(self)
+        self.code_text = CodeText(self)
         self.visualiser = wx.Panel(self)
         self.code_runner = wx.Panel(self)
 
@@ -27,11 +25,7 @@ class EditorPage(wx.Panel):
         self.aui_manager.Update()
 
 
-
 class EditorNotebook(wx.aui.AuiNotebook):
-    # https://wiki.wxpython.org/AuiNotebook
-    # https://wxpython.org/Phoenix/docs/html/wx.aui.AuiNotebook.html
-    # https://wxpython.org/Phoenix/docs/html/wx.lib.agw.aui.auibook.AuiNotebook.html
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, style=wx.aui.AUI_NB_DEFAULT_STYLE)
