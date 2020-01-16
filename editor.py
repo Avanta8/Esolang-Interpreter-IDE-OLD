e8c2baa3dcc829df79963b026eaf8d425f12f5ef
Harry Min <happyjet123@gmail.com>
2020-01-16 18:02:19 +0000

    Created editor notebook

--- editor.py
+++ editor.py
@@ -6,8 +6,26 @@ import wx.aui
 # Could have one dockwidget for the whole editor area
 
 
-class TextEditor(wx.Panel):
-    pass
+class EditorPage(wx.Panel):
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.create_widgets()
+    
+    def create_widgets(self):
+
+        self.code_text = wx.TextCtrl(self)
+        self.visualiser = wx.Panel(self)
+        self.code_runner = wx.Panel(self)
+
+        self.aui_manager = wx.aui.AuiManager(self, wx.aui.AUI_MGR_DEFAULT | wx.aui.AUI_MGR_LIVE_RESIZE)
+        self.aui_manager.AddPane(self.code_text, wx.CENTRE, 'Code Text')
+        self.aui_manager.AddPane(self.visualiser, wx.TOP, 'Visualiser')
+        self.aui_manager.AddPane(self.code_runner, wx.BOTTOM, 'Code Runner')
+        self.aui_manager.Update()
+
 
 
 class EditorNotebook(wx.aui.AuiNotebook):
@@ -16,12 +34,14 @@ class EditorNotebook(wx.aui.AuiNotebook):
     # https://wxpython.org/Phoenix/docs/html/wx.lib.agw.aui.auibook.AuiNotebook.html
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs, style=wx.aui.AUI_NB_DEFAULT_STYLE)
 
         # Just quick stuff to check everything works
 
-        self.AddPage(TextEditor(self), 'Window1', True)
-        self.AddPage(TextEditor(self), 'Window2', True)
+        self.AddPage(EditorPage(self), 'Window1', True)
+        self.AddPage(EditorPage(self), 'Window2', True)
+        self.AddPage(EditorPage(self), 'Window2', True)
+        self.AddPage(EditorPage(self), 'Window2', True)
 
 
 class EditorArea(wx.Panel):
