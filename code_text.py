9d01ba38ce706ded15ec551ef0458da87cdaf45d
Harry Min <happyjet123@gmail.com>
2020-01-17 19:26:08 +0000

    Basic multicursor support

--- code_text.py
+++ code_text.py
@@ -16,7 +16,9 @@ class CodeText(wx.stc.StyledTextCtrl):
 
         self.SetLexer(wx.stc.STC_LEX_PYTHON)
         self.SetMarginType(1, wx.stc.STC_MARGIN_NUMBER)
+
         self.SetCaretLineVisible(True)
+        self.SetAdditionalSelectionTyping(True)
 
         self._set_default_indentation_settings()
         self._edit_keybinds()
@@ -35,14 +37,47 @@ class CodeText(wx.stc.StyledTextCtrl):
 
         self.CmdKeyClear(ord('U'), wx.stc.STC_SCMOD_CTRL)
         self.CmdKeyClear(ord('U'), wx.stc.STC_SCMOD_CTRL | wx.stc.STC_SCMOD_SHIFT)
+        # self.CmdKeyAssign(65, wx.stc.STC_SCMOD_NORM, lambda: print)
 
-        self.Bind(wx.EVT_KEY_DOWN, self.key_pressed)
+        self.Bind(wx.EVT_KEY_DOWN, self.key_pressed_event)
+        self.Bind(wx.EVT_LEFT_DOWN, self.mouse_left_down_event)
+        self.Bind(wx.EVT_MOTION, self.mouse_move_event)
 
-    def key_pressed(self, event):
+    def key_pressed_event(self, event):
         key_code = event.GetKeyCode()
+        modifiers = event.GetModifiers()
 
-        if key_code == wx.WXK_RETURN and not wx.GetKeyState(wx.WXK_SHIFT):
+        if key_code == wx.WXK_RETURN and modifiers == wx.MOD_NONE:
             self.newline_with_indentation()
+        else:
+            event.Skip()
+
+    def mouse_left_down_event(self, event):
+        modifiers = event.GetModifiers()
+
+        self.SetFocus()
+
+        pos = self.PositionFromPoint(event.GetPosition())
+        if modifiers == wx.MOD_CONTROL:
+            self.AddSelection(pos, pos)
+        else:
+            self.ClearSelections()
+            self.SetSelection(pos, pos)
+
+    def mouse_move_event(self, event):
+        modifiers = event.GetModifiers()
+
+        if event.LeftIsDown():
+            print('ctrl + mouse drag')
+            selection = self.GetMainSelection()
+            pos = self.PositionFromPoint(event.GetPosition())
+            anchor = self.GetSelectionNAnchor(selection)
+            if pos < anchor:
+                self.SetSelectionNStart(selection, pos)
+            else:
+                self.SetSelectionNEnd(selection, pos)
+            self.SetSelectionNAnchor(selection, anchor)
+            self.SetSelectionNCaret(selection, pos)
         else:
             event.Skip()
 
